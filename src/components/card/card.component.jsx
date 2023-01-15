c45aeddbabef6a30c555ab5a7dddeb3bdcf42665
marcelohcortez <marcelohcortez@gmail.com>
2023-01-15 17:52:21 +0000

    Add conditional rendering for image

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -4,12 +4,15 @@ import "./card.style.css"
 
 const Card = ({ seriesItem }) => {
     const navigate = useNavigate();
-    
+    const {externals, image, name, rating} = seriesItem.show
+
     return (
-        <div className="card-container" onClick={() => navigate(`/details/${seriesItem.show.externals.thetvdb}`)}>
-            <img className="card-image" src={seriesItem.show.image.medium}/>
-            <p className="card-name">{seriesItem.show.name}</p>
-            <span className="card-rating">Rating: {seriesItem.show.rating.average}</span>
+        <div className="card-container" onClick={() => navigate(`/details/${externals.imdb}`)}>
+            {image !== null &&
+                <img className="card-image" src={image.medium}/>
+            }
+            <p className="card-name">{name}</p>
+            <span className="card-rating">Rating: {rating.average}</span>
         </div>
     )
 }
